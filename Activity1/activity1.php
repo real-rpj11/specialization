ad1aa7876d8e2b27b73df0ee7ecfa919b0b1ad65
1styrGroupA <1styrGroupA@pn.local>
2021-01-11 13:06:24 +0800

    activity 1

---
+++ Activity1/activity1.php
@@ -0,0 +1,16 @@
+<?php
+// 1
+ print("Hello, World!<br>");
+
+//  2 part 1
+$name = "Precy Jane";
+$age = "20";
+echo "Hello $name. You are $age years old.<br>";
+
+// 2 part 2
+ $x = 18;
+ $y = 5;
+ $sum = $x + $y;
+ echo $sum;
+ 
+?>
